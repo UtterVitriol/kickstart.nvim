3c9f119ef358d536bd1a38f21ac093f4a6b8a98d
uttervitriol <uttervitriol@gmail.com>
2024-08-25 10:15:08 -0400

    nercommenter for more comment options \ maybe fix clangd

--- init.lua
+++ init.lua
@@ -283,6 +283,9 @@ require('lazy').setup({
     },
   },
 
+  {
+    'preservim/nerdcommenter',
+  },
   -- Cool close for brackets, quites, etc.
   -- {
   --   'm4xshen/autoclose.nvim',
@@ -696,9 +699,40 @@ require('lazy').setup({
         pyright = {},
         flake8 = {},
         clangd = {
+          -- I made these changes because clangd would crash every once in a while and
+          -- I couldn't really figure it out. I haven't yet had a crash with these changes.
+          keys = {
+            { '<leader>ch', '<cmd>ClangdSwitchSourceHeader<cr>', desc = 'Switch Source/Header (C/C++)' },
+          },
+          root_dir = function(fname)
+            return require('lspconfig.util').root_pattern(
+              'Makefile',
+              'configure.ac',
+              'configure.in',
+              'config.h.in',
+              'meson.build',
+              'meson_options.txt',
+              'build.ninja'
+            )(fname) or require('lspconfig.util').root_pattern('compile_commands.json', 'compile_flags.txt')(fname) or require('lspconfig.util').find_git_ancestor(
+              fname
+            )
+          end,
+          capabilities = {
+            offsetEncoding = { 'utf-16' },
+          },
           cmd = {
             'clangd',
-            '-header-insertion=never',
+            '--background-index',
+            '--clang-tidy',
+            '--header-insertion=never',
+            '--completion-style=detailed',
+            '--function-arg-placeholders',
+            '--fallback-style=llvm',
+          },
+          init_options = {
+            usePlaceholders = true,
+            completeUnimported = true,
+            clangdFileStatus = true,
           },
         },
         -- rust_analyzer = {},
